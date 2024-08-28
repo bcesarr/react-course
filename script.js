0e5abe766d357dcfa3cb4a43f51f1b6a00feaf5c
bcesar7 <brunocesar.as7@gmail.com>
2024-08-28 18:30:14 -0300

    add: arquivo JS da aula adicionado

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Estou funcionando")
+// console.log("Estou funcionando!")
+
+function SomaNumeros(n1,n2){
+    return n1+n2
+}
+
+// console.log(SomaNumeros(1, 4))
+
+const person = {
+    name: "Lyan",
+    age: 18,
+    gender: "Male"
+}
+
+// Forma estruturada
+// console.log(person)
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person.gender)
+
+
+// Forma desestruturada
+const {name, age, gender} = person
+
+console.log(name)
+console.log(age)
+console.log(gender)
