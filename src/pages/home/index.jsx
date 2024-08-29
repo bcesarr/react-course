55320a0a92c1f83dd84cc561984a651d29ae40d2
bcesar7 <brunocesar.as7@gmail.com>
2024-08-29 18:40:06 -0300

    atu: avançando no projeto, unificando os componentes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,22 @@
-import { Header, Sections } from "../../components";
+import Header from "../../components/header";
+import Sections from "../../components/sections"
+import Card from "../../components/card"
+import Cubo from "../../assets/cubo.svg"
 
 export default function Home() {
     return (
         <body>
             <Header />
             <Sections />
-            
+            <Card
+                image={Cubo}
+
+                imgAlt="Imagem de um cubo pequeno"
+
+                paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla."
+
+            />
+
         </body>
     )
 }
