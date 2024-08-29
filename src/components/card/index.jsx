05bdacb2f8fd4ef7d8bc03d1a6eae28e08967b45
bcesar7 <brunocesar.as7@gmail.com>
2024-08-29 11:39:04 -0300

    add: novo componente adicionado: card

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,22 +1,113 @@
 import './style.css'
 
-export default function Card({title, type, typeTwo, Weaknesses, description, image, imgAlt}){
-    return (
-      <div className='card'>
-        <img src={image} alt={imgAlt}/>
-        <div className='text-wrapper'>
-          <h2>{title}</h2>
-          <div className='type'>
-            <h3>Tipo:</h3>
-            <p>{type}</p>
-            <p><span>{typeTwo}</span></p>
-          </div>
-          <div className='Weaknesses'>
-            <h3>Fraquezas:</h3>
-            <p>{Weaknesses}</p>
-          </div>
-          <p><b>Descrição: </b>{description}</p>
+// Está importação da imagem abaixo é para o modelo do módo tradicional
+// import cubo from '../../assets/cubo.svg'
+
+// ----------  Card do Pokemon  ----------\\
+// export default function Card({title, type, typeTwo, Weaknesses, description, image, imgAlt}){
+//     return (
+//       <div className='card'>
+//         <img src={image} alt={imgAlt}/>
+//         <div className='text-wrapper'>
+//           <h2>{title}</h2>
+//           <div className='type'>
+//             <h3>Tipo:</h3>
+//             <p>{type}</p>
+//             <p><span>{typeTwo}</span></p>
+//           </div>
+//           <div className='Weaknesses'>
+//             <h3>Fraquezas:</h3>
+//             <p>{Weaknesses}</p>
+//           </div>
+//           <p><b>Descrição: </b>{description}</p>
+//         </div>
+//       </div>
+//     )
+//   }
+
+
+
+// ----------  Forma de fazer os cards, do jeito mais tradicional  ---------- \\
+
+// export default function Card() {
+//   return (
+//     <section className='section-cards'>
+
+//         <div className='div-cards-global'>
+
+//           <div className='card-infos'>
+//             <img src={cubo} alt="Imagem de um cubo" />
+
+//             <h3>Highlight benefit one</h3>
+
+//             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br /> Suspendisse varius enim in eros elementum tristique.<br />Duis cursus, mi quis viverra ornare, eros dolor interdum<br />nulla.</p>
+
+//           </div>
+
+//           <div className='card-infos'>
+//             <img src={cubo} alt="Imagem de um cubo" />
+
+//             <h3>Highlight benefit two</h3>
+
+//             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br /> Suspendisse varius enim in eros elementum tristique.<br />Duis cursus, mi quis viverra ornare, eros dolor interdum<br />nulla.</p>
+
+//           </div>
+
+//           <div className='card-infos'>
+//             <img src={cubo} alt="Imagem de um cubo" />
+
+//             <h3>Highlight benefit three</h3>
+
+//             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br /> Suspendisse varius enim in eros elementum tristique.<br />Duis cursus, mi quis viverra ornare, eros dolor interdum<br />nulla.</p>
+
+//           </div>
+//         </div>
+
+//     </section>
+//   )
+// }
+
+
+// ----------------------------------------------------------------------- \\
+
+
+// Forma de fazer utilizando as Props
+
+export default function Card({paragraph, image, imgAlt}) {
+  return (
+    <section className='section-cards'>
+
+      <div className='div-cards-global'>
+
+        <div className='card-infos'>
+          <img src={image} alt={imgAlt}/>
+
+          <h3>Highlight benefit one</h3>
+
+          <p>{paragraph}</p>
+
         </div>
+        
+        <div className='card-infos'>
+          <img src={image} alt={imgAlt}/>
+
+          <h3>Highlight benefit two</h3>
+
+          <p>{paragraph}</p>
+
+        </div>
+        
+        <div className='card-infos'>
+          <img src={image} alt={imgAlt}/>
+
+          <h3>Highlight benefit three</h3>
+
+          <p>{paragraph}</p>
+
+        </div>
+
       </div>
-    )
-  }
+
+    </section>
+  )
+}
