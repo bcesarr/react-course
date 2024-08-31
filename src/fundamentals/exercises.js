6060696c2143c6520ba11b41ba96388c67624f1f
bcesar7 <brunocesar.as7@gmail.com>
2024-08-31 13:54:22 -0300

    feat: update exercises of the JS with Operator Ternarie, If, Else and Switch

--- src/fundamentals/exercises.js
+++ src/fundamentals/exercises.js
@@ -130,3 +130,64 @@
 
 // ----- Exemplo de Uso Switch ----- \\
 
+// function diaDaSemana(dia) {
+//     switch (dia) {
+//         case 1: return console.log("Domingo")
+//         case 2: return console.log("Segunda")
+//         case 3: return console.log("Terça")
+//         case 4: return console.log("Quarta")
+//         case 5: return console.log("Quinta")
+//         case 6: return console.log("Sexta")
+//         case 7: return console.log("Sábado")
+//         default: return console.log("Dia inválido")   // Todo Switch tem que ter o default no final dele
+//     }
+// }
+
+// diaDaSemana(8)
+
+
+
+// ----- Exercicio 1 ----- \\
+
+// Crie uma função que recebe um número de 1 a 12 e retorne o nome do mês correspondente.
+
+// function verificarMes (mes) {
+//     switch (mes) {
+//         case 1: return console.log("Janeiro")
+//         case 2: return console.log("Fevereiro")
+//         case 3: return console.log("Março")
+//         case 4: return console.log("Abril")
+//         case 5: return console.log("Maio")
+//         case 6: return console.log("Junho")
+//         case 7: return console.log("Julho")
+//         case 8: return console.log("Agosto")
+//         case 9: return console.log("Setembro")
+//         case 10: return console.log("Outubro")
+//         case 11: return console.log("Novembro")
+//         case 12: return console.log("Dezembro")
+//         default: return console.log("Mês inválido")
+//     }
+// }
+
+// verificarMes(12)
+
+
+
+// ----- Exercicio 2 ----- \\
+
+// Crie uma função que recebe uma string (
+//     ```
+//     "iniciante" , "intermediário" , "avançado"
+//     ```
+//) e retorne uma mensagem apropriada de acordo com o nível.
+
+function handleDev(cargo) {
+    switch (cargo) {
+        case 'iniciante': return console.log("Vai estudar")
+        case 'intermediario': return console.log("Vai estudar o foco das vagas")
+        case 'avancado': return console.log("Top")
+        default: return console.log("Valor digitado inválido")
+    }
+}
+
+handleDev("intermediario")
